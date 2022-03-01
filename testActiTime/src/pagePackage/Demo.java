37c76c7eee5f165ad2a2c1426317cd48474dbcc1
Anu <Anu@ANANYAKOLTE>
2022-03-01 09:19:22 +0530

    first code

---
+++ testActiTime/src/pagePackage/Demo.java
@@ -0,0 +1,14 @@
+package pagePackage;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("First code");
+
+	}
+
+}
